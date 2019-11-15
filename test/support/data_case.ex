104551941bf3bec7794803637c2cf4de7cfd1916
Chaz Watkins <chazwatkins@live.com>
2019-11-15 15:41:20 -0600

    add test fixtures

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -24,6 +24,7 @@ defmodule AcolyteTrials.DataCase do
       import Ecto.Changeset
       import Ecto.Query
       import AcolyteTrials.DataCase
+      import AcolyteTrials.TestHelpers
     end
   end
 
---
+++ test/support/test_helpers.ex
@@ -0,0 +1,26 @@
+defmodule AcolyteTrials.TestHelpers do
+  alias AcolyteTrials.{Accounts, Devices}
+
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "Some User",
+        github_username: "user#{System.unique_integer([:positive])}"
+      })
+      |> Accounts.create_user()
+
+    user
+  end
+
+  def insert_device(attrs \\ %{}) do
+    {:ok, device} =
+      attrs
+      |> Enum.into( %{
+        config_hash: "12345678910"
+      })
+      |> Devices.create_device()
+
+    device
+  end
+end
